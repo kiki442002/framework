6192b3065bf7bca9c0a7898c0021919e3647efde
poubzs <poubzs.thugzs@gmail.com>
2023-05-27 17:26:01 +0200

    fonction inutile (normalement)

--- src/app/computer.service.ts
+++ src/app/computer.service.ts
@@ -18,44 +18,12 @@ export class ComputerService {
     this.getComputers().subscribe(
       (response: Computer[]) => {
         this.computers = response;
-        this.createCardComponents();
+        console.log(this.computers); // You can access the fetched data here
       },
       (error: any) => {
         console.error('Error fetching computers:', error);
       }
     );
-  }
-
-  createCardComponents() {
-    const parentElement = document.getElementById('bottom');
-  
-    if (!parentElement) {
-      console.error("Parent element 'bottom' not found.");
-      return;
-    }
-  
-    this.computers.forEach(computer => {
-      const cardElement = document.createElement('div');
-      cardElement.className = 'card';
-  
-      const imgElement = document.createElement('img');
-      imgElement.className = 'card';
-      imgElement.src = '../../assets/images/pc_test.jpg';
-      imgElement.alt = 'pc_test';
-      cardElement.appendChild(imgElement);
-  
-      const h3Element = document.createElement('h3');
-      h3Element.className = 'feature';
-      h3Element.textContent = computer.nom;
-      cardElement.appendChild(h3Element);
-  
-      const systemElement = document.createElement('div');
-      systemElement.className = 'feature';
-      systemElement.textContent = `RAM: ${computer.system.ram} Go, CPU: ${computer.system.cpu.nom}, HDD: ${computer.system.hdd.capacite} Go`;
-      cardElement.appendChild(systemElement);
-  
-      parentElement.appendChild(cardElement);
-    });
   }
 }
 
